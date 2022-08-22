f03d79adc5bfd1995dfa72bd7a71129a532e639d
Karl Du <18556906294@163.com>
2022-08-22 16:52:27 +0800

    swagger

--- src/server/Sixpence.WeChat/Startup.cs
+++ src/server/Sixpence.WeChat/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,11 +18,27 @@ namespace Sixpence.WeChat
         public override void ConfigureServices(IServiceCollection services)
         {
             base.ConfigureServices(services);
+
+#if DEBUG
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "接口文档", Version = "v1" });
+            });
+#endif
         }
 
         public override void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHttpContextAccessor accessor)
         {
             base.Configure(app, env, accessor);
+
+#if DEBUG
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"v1/swagger.json", "接口文档");
+                c.RoutePrefix = "Swagger";
+            });
+#endif
         }
     }
 }
